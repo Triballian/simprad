3ba830e8fcc05bd118f8e2ebb01cc9b7c7012f69
Triballian <candiapps@gmail.com>
2016-03-19 23:08:56 -0500

    Fixed Sergio's Integral typo

--- src/main.py
+++ src/main.py
@@ -6,5 +6,32 @@ http://stackoverflow.com/questions/31217274/python-simplifying-radicals-not-goin
 @author: Noe
 '''
 
+from math import modf
+from math import sqrt
+#Establish input
+def sqrtinput():
+    
+    A = int(raw_input("Enter the number under the radical:"))
+    
+    #Input taken, setup math
+    B = (A + 1)
+    C = sqrt(B)
+    #seperate parts
+    Fractional, Integral = modf(C)
+    Fractional1, Integral1 = modf(A / (Integral**2))
+    def Intg(Integral):
+        while Fractional1 == Fractional1:
+            if Fractional1 == 0 and (A / (Integral**2)) == 1:
+                print (Integral)
+                return Integral
+            else:
+                Integral = (Integral - 1)
+    if Integral == 1:
+        print (A / (Integral**2))
+    elif not Integral ==1 or (Fractional1 == 0 and (A / (Integral**2)) == 1):
+        print Integral, (A / (Integral**2))
+    
 if __name__ == '__main__':
+    sqrtinput.A()
+    
     pass
