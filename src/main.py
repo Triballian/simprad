6b0ea18b7e214c5126b862cfa773d8de17e051fd
Triballian <candiapps@gmail.com>
2016-03-19 19:23:15 -0500

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+'''
+Created on Mar 19, 2016
+insprired by Curious Cheetah and Sergio
+http://curiouscheetah.com/BlogMath/simplify-radicals-python-code/
+http://stackoverflow.com/questions/31217274/python-simplifying-radicals-not-going-well
+@author: Noe
+'''
+
+if __name__ == '__main__':
+    pass
